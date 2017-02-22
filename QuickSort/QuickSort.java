1a7e56c96593e65de7c943cb3c8ba8a7b21d28ab
Mauricio <maubot01@gmail.com>
2017-02-22 19:33:23 -0400

    QuickSort

---
+++ QuickSort/QuickSort.java
@@ -0,0 +1,34 @@
+public class QuickSort2 {
+
+   public static void quickSort(int vec[], int inicio, int fin){
+                if(inicio>=fin) return;
+                int pivote=vec[inicio];
+                int elemIzq=inicio+1;
+                int elemDer=fin;
+                while(elemIzq<=elemDer){
+                        while(elemIzq<=fin && vec[elemIzq]<pivote){
+                                elemIzq++;
+                        }
+                        while(elemDer>inicio && vec[elemDer]>=pivote){
+                                elemDer--;
+                        }
+                        if(elemIzq<elemDer){
+                                int temp=vec[elemIzq];
+                                vec[elemIzq]=vec[elemDer];
+                                vec[elemDer]=temp;
+                        }
+                }
+                
+                if(elemDer>inicio){
+                        int temp=vec[inicio];
+                        vec[inicio]=vec[elemDer];
+                        vec[elemDer]=temp;
+                }
+                quickSort(vec, inicio, elemDer-1);
+                quickSort(vec, elemDer+1, fin);
+        }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
